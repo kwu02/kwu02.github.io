a04865ebae90dfa5ae171b0881c37a6b9e16b786
kwu02 <143127740+kwu02@users.noreply.github.com>
2023-10-25 12:11:06 -0700

    Add files via upload

--- cnit133a/angular/src/app/products/products.component.ts
+++ cnit133a/angular/src/app/products/products.component.ts
@@ -1,1 +1,13 @@
+import { Component } from '@angular/core';
+import productData from '../products.json';
 
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+
+export class ProductsComponent {
+  title = 'Angular-JSON';
+  Products: any = productData;
+}
